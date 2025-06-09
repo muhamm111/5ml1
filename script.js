434a24699aa510beceb1cf3e30def6e05845c3b0
muhamm111 <muhammadaminulugbekov20@gmail.com>
2025-06-09 11:58:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// console.log("USTOZ 1000 COIN QOYIB BERIN MASHINI KILGANIN UCHUN!!!!!")
+
+// var student1 = "odil"
+// let student2 = "akil"
+// const student3 = "yunus"
+ 
+// alert("")
+// confirm("")
+
+let name = prompt("Ведите ваше имя")
+let surname = prompt("Ведите вашу фамилию")
+let age = prompt("Ведите ваш возраст")
+let city = prompt("Где вы живете?")
+let subject = prompt("Какой ваш любимый предмет?")
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(city);
+console.log(subject);
+
+// alert('Ozi xaqida xichkim bilmidi dip olangan oquvchi ' + name + '\n unga yoqadigan oquvchi' + like + '🐒 u yoqtirmaydigan oquvchi esa' + donlike);
